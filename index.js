ccc609909bb17a6598f92d1af18b9e86e3e7e17a
oangia <hqnhatdn@gmail.com>
2026-04-19 16:42:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = goodmusic({
 })
 
 app.use(cors());
-
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use('/', require('./routes/songs'));
 
