0490bec68c5713cb01d46e3b29e4f1fafb91afae
oangia <hqnhatdn@gmail.com>
2026-04-19 10:04:34 +0000

    U

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const goodmusic = require("goodmusic");
-const express = require("express");
 const cors = require("cors");
 
 const app = goodmusic({
@@ -7,8 +6,6 @@ const app = goodmusic({
 })
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use('/', require('./routes/songs'));
 
